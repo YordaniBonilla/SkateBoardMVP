d413c112992265aa10145e8c970c57fd7f1d8fe8
YordaniBonilla <b.yordani93@gmail.com>
2019-11-03 00:23:14 -0600

    added link to images instead of trick title

--- react-client/src/components/Trick.jsx
+++ react-client/src/components/Trick.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './Trick.css';
+import styles from './Trick.css';
 
 const Trick = (props) => (
   <div>
   <ol>
      <li style={{display:"flex",flexDirection:"column"}}>
-       <a href={props.redirect}>{props.trickName}</a>
-       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR6A0934fqdhTrho9yCmEBgO1vDRPPA_ws3rdGIfZd-MMYAjwHs" height="200px" width="auto"/>
+       <p className={styles.color}>{props.trickName}</p>
+       <a  href={props.redirect}>
+         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR6A0934fqdhTrho9yCmEBgO1vDRPPA_ws3rdGIfZd-MMYAjwHs" height="200px" width="auto"/>
+       </a>
        <h4>{props.id}</h4>
        <button>Delete</button>
      </li>
